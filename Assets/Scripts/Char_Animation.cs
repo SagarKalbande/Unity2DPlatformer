17f97b2b7a59551049ce4019f2fd6d1446e2a1d3
Sagar Kalbande <sagar@vizexperts.com>
2018-05-11 17:28:31 +0530

    Converted to prefab. Added movement controls to char_Animationscript

--- Assets/Scripts/Char_Animation.cs
+++ Assets/Scripts/Char_Animation.cs
@@ -5,32 +5,47 @@ using UnityEngine;
 public class Char_Animation : MonoBehaviour {
 
     Animator CharacterAnimator;
+    Rigidbody2D Body;
     bool LookingRight = true;
 
 	void Start () {
         CharacterAnimator = GetComponent<Animator>();
+        Body = GetComponent<Rigidbody2D>();
 	}
 	
 
 	void Update ()
     {
+        if (Input.GetKeyDown("up"))
+        {
+            Body.velocity = new Vector3(0.0f, 10.0f, 0.0f);
+        }
 
         if (Input.GetKey("right"))
         {
             if (!LookingRight)
             {
                 gameObject.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 180);
+                LookingRight = true;
             }
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0.1f, 0.0f, 0.0f);
             CharacterAnimator.SetBool("Run", true);
         }
         else if (Input.GetKey("left"))
         {
+            if (LookingRight)
+            {
+                gameObject.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 180);
+                LookingRight = false;
+            }
+            gameObject.transform.position = gameObject.transform.position + new Vector3(-0.1f, 0.0f, 0.0f);
             CharacterAnimator.SetBool("Run", true);
         }
         else if (Input.GetKey("down"))
         {
             CharacterAnimator.SetBool("Slide", true);
         }
+ 
         else if (Input.GetKey("z"))
         {
             CharacterAnimator.SetBool("Meele", true);
